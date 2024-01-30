df81e939ccec84b2904dea283dbe030683c5f26c
Vanshika Aggarwal <141432105+vanshika-aggarwal1@users.noreply.github.com>
2024-01-30 18:38:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+function toggleSun(){
+    document.body.classList.toggle("togglebody");
+    document.getElementById("sun").classList.toggle("moon");
+    document.getElementById("sun").classList.toggle("sun");
+}
